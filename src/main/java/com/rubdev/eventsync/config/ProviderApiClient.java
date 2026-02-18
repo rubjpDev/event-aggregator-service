25261c9d866467239454be3b5c5873b7e417fd7c
rubjpDev <rubenjpdev@outlook.com>
2026-02-18 16:40:29 +0100

    MDC + Logstash json + logback
    TRAZABILIDAD GARANTIZADA

--- src/main/java/com/rubdev/eventsync/config/ProviderApiClient.java
+++ src/main/java/com/rubdev/eventsync/config/ProviderApiClient.java
@@ -15,21 +15,21 @@ public class ProviderApiClient {
 
     private final RestTemplate restTemplate;
     private static final int MAX_RETRIES = 5;
-    private static final long RETRY_DELAY = 5000; //THIS NUMBER IN MS
+    private static final long RETRY_DELAY = 5000; // THIS NUMBER IN MS
 
-    public String fetchProviderEvents(){
-        for (int attempt = 1; attempt <= MAX_RETRIES;attempt++){
+    public String fetchProviderEvents() {
+        for (int attempt = 1; attempt <= MAX_RETRIES; attempt++) {
             try {
-                log.info("XML FETCH -- RETRY NUMBER: " + attempt);
+                log.info("XML FETCH -- RETRY NUMBER: {}", attempt);
                 return restTemplate.getForObject(ConnectionUtils.PROVIDER_URL.getValue(), String.class);
-            }catch (HttpServerErrorException e){
-                log.warn("PROVIDER ERROR: " + e.getStatusCode());
-            }catch (RestClientException r){
-                log.warn("CONNECTION ERROR: "+r.getMessage());
+            } catch (HttpServerErrorException e) {
+                log.warn("PROVIDER ERROR: {}", e.getStatusCode());
+            } catch (RestClientException r) {
+                log.warn("CONNECTION ERROR: {}", r.getMessage());
             }
             try {
                 Thread.sleep(RETRY_DELAY);
-            }catch (InterruptedException ie){
+            } catch (InterruptedException ie) {
                 Thread.currentThread().interrupt();
                 log.error("-- THREAD ERROR, RETRY INTERRUPTED --");
                 break;
